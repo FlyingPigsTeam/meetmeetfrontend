1d70d6625cfd02dbe3972c9e784d0ee2930b5089
m.hoseein mirzaei <1380mirza@gmail.com>
2023-04-02 03:07:42 +0430

    chore: design and check functionality of filter am=nd sort in HomePage

--- src/pages/Home/Filter.js
+++ src/pages/Home/Filter.js
@@ -1,16 +1,18 @@
 import { Fragment, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
-var asc_color = "bg-sky-500";
-var des_color = " ";
+// var asc_color = "bg-sky-500";
+// var des_color = " ";
 
 export default function Filters() {
   const [category, setcategory] = useState([]);
   const [startdate, setstartdate] = useState("");
   const [enddate, setenddate] = useState("");
+  const [starttime, setstarttime] = useState("");
+  const [endtime, setendtime] = useState("");
   const [capacity, setcapacity] = useState(0);
   const [sort, setSort] = useState("None");
-  const [asc, setasc] = useState(1);
+  // const [asc, setasc] = useState(1);
 
   function handlecategory(e) {
     const { value, checked } = e.target;
@@ -23,14 +25,14 @@ export default function Filters() {
       });
     }
   }
-  function handleasc() {
-    setasc(1);
-    //console.log(asc);
-  }
-  function handledes() {
-    setasc(0);
-    //console.log("sdjksndvb");
-  }
+  // function handleasc() {
+  //   setasc(1);
+  //   //console.log(asc);
+  // }
+  // function handledes() {
+  //   setasc(0);
+  //   //console.log("sdjksndvb");
+  // }
 
   // const handle_start_date = (e) => {
   //   const get_startdate_value = e.target.value;
@@ -88,86 +90,107 @@ export default function Filters() {
       ],
     },
   ];
-  const notificationMethods = [
-    { id: "Time", title: "Time" },
-    { id: "Duration", title: "Duration" },
-    { id: "Capacity", title: "Capacity" },
-    { id: "None", title: "None" },
-  ];
+  // const notificationMethods = [
+  //   { id: "Time", title: "Time" },
+  //   { id: "Duration", title: "Duration" },
+  //   { id: "Capacity", title: "Capacity" },
+  //   { id: "None", title: "None" },
+  // ];
   console.log("category : ", category);
   console.log("startdate : ", startdate);
   console.log("enddate : ", enddate);
+  console.log("starttime : ", starttime);
+  console.log("endtime : ", endtime);
   console.log("capacity : ", capacity);
   console.log("sort : ", sort);
-  console.log("asc : ", asc);
-  if (asc) {
-    asc_color = "bg-sky-500";
-    des_color = " ";
-  } else {
-    asc_color = " ";
-    des_color = "bg-sky-500";
-  }
+  // console.log("asc : ", asc);
+  // if (asc) {
+  //   asc_color = "bg-sky-500";
+  //   des_color = " ";
+  // } else {
+  //   asc_color = " ";
+  //   des_color = "bg-sky-500";
+  // }
 
   return (
     <div>
       <div className="bg-white w-11/12">
         {/* Filters */}
         <section aria-labelledby="filter-heading">
-          <div className="border-b border-myGrey1 bg-myDark1 pb-4">
-            <div className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+          <div className="border-b border-myGrey1 bg-myDark1 pb-0">
+            <div className="flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
               <div className="date">
-                <div date-rangepicker className="flex items-center">
-                  <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-                    <label>Start Date :</label>
+                <div className="flex items-center -ml-8">
+                  <label className="inline text-1xl mr-0  mb-7  font-medium text-myGrey">
+                    Duration:
+                  </label>
+                  <div className="relative inset-y-0 left-0 flex items-center pl-3  ">
                     <input
-                      name="start"
+                      name="startDate"
                       type="Date"
-                      className="bg-myDark1 border-4 border-myGrey1 text-myGrey1 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-myDark1 border-myGrey rounded-md mb-7 py-2  text-myGrey text-sm  focus:ring-myBlueGreen1 focus:border-myBlueGreen1 block w-full pl-10 p-2.5  dark:bg-myGrey dark:border-myGrey dark:placeholder-myGrey dark:text-myGrey dark:focus:ring-myBlueGreen1 dark:focus:border-myDark2"
                       placeholder="Select date start"
                       onChange={(e) => setstartdate(e.target.value)}
                     />
                   </div>
-                  <span className="mx-4 mt-6 text-gray-500">to</span>
-                  <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-                    <label>End Date :</label>
+                  <div className="relative inset-y-0 left-0 flex items-center pl-3  ">
                     <input
-                      name="end"
-                      type="Date"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Select date end"
+                      name="startTime"
+                      type="time"
+                      className="bg-myDark1 border-myGrey rounded-md mb-7 py-2  text-myGrey text-sm  focus:ring-myBlueGreen1 focus:border-myBlueGreen1 block w-full pl-10 p-2.5  dark:bg-myGrey dark:border-myGrey dark:placeholder-myGrey dark:text-myGrey dark:focus:ring-myBlueGreen1 dark:focus:border-myDark2"
+                      placeholder="Select time start"
                       onChange={(e) => setenddate(e.target.value)}
+                    />
+                  </div>
+                  <span className="mb-7 py-2 -mr-3 text-myGrey">to</span>
+                  <div className="relative inset-y-0 left-0 flex items-center pl-3  ">
+                    <input
+                      name="endDate"
+                      type="Date"
+                      className="bg-myDark1 border-myGrey rounded-md mb-7 py-2  text-myGrey text-sm  focus:ring-myBlueGreen1 focus:border-myBlueGreen1 block w-full pl-10 p-2.5  dark:bg-myGrey dark:border-myGrey dark:placeholder-myGrey dark:text-myGrey dark:focus:ring-myBlueGreen1 dark:focus:border-myDark2"
+                      placeholder="Select date end"
+                      onChange={(e) => setstarttime(e.target.value)}
+                    />
+                  </div>
+                  <div className="relative inset-y-0 left-0 flex items-center pl-3  ">
+                    <input
+                      name="endtime"
+                      type="time"
+                      className="bg-myDark1 border-myGrey rounded-md mb-7 py-2  text-myGrey text-sm  focus:ring-myBlueGreen1 focus:border-myBlueGreen1 block w-full pl-10 p-2.5  dark:bg-myGrey dark:border-myGrey dark:placeholder-myGrey dark:text-myGrey dark:focus:ring-myBlueGreen1 dark:focus:border-myDark2"
+                      placeholder="Select time end"
+                      onChange={(e) => setendtime(e.target.value)}
                     />
                   </div>
                 </div>
               </div>
-              <div className="room-capacity">
-                <div className="relative">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-                  <label>Room's Capacity :</label>
-                  <input
-                    name="room-capacity"
-                    type="text"
-                    pattern="^[0-9]+$"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Enter Room's capacity"
-                    onBlur={(e) => setcapacity(e.target.value)}
-                  />
-                </div>
+              <div>
+                <label className="inline text-1xl font-medium text-myGrey">
+                  Capacity:
+                </label>
+                <select
+                  className="inline rounded-md border-myGrey py-2 pl-3 text-1xl mb-7 ml-2 -mr-20 font-bold hover:font-bold w-30 pr-10 bg-myDark1 text-myGrey text-base focus:border-myBlueGreen1 focus:outline-none focus:ring-myBlueGreen1 sm:text-sm"
+                  defaultValue="None"
+                  value={capacity}
+                  onChange={(e) => setcapacity(e.target.value)}
+                >
+                  <option value="None">Doesn't Matter</option>
+                  <option value="1-10">1-10</option>
+                  <option value="10-20">10-20</option>
+                  <option value="20<">More than 20</option>
+                </select>
               </div>
               <div className="hidden sm:block">
                 <div className="flow-root">
-                  <Popover.Group className="-mx-4 flex items-center divide-x divide-gray-200">
+                  <Popover.Group className="ml-10 mb-4 flex items-center divide-x divide-myGrey">
                     {filters.map((section) => (
                       <Popover
                         key={section.name}
-                        className="relative inline-block px-4 text-left"
+                        className="relative inline-block ml-3 text-left"
                       >
-                        <Popover.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                        <Popover.Button className="group border rounded-md py-1 pl-7 mb-3 -mr-7 ml-9 font-bold inline-flex justify-center text-1xl font-lg text-myGrey hover:text-myGrey">
                           <span>{section.name}</span>
                           <ChevronDownIcon
-                            className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                            className="-mr-1 ml-1 h-7  w-10 flex-shrink-0 text-myGrey group-hover:text-mygrey"
                             aria-hidden="true"
                           />
                         </Popover.Button>
@@ -181,8 +204,8 @@ export default function Filters() {
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Popover.Panel className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white p-4 shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <form className="space-y-4">
+                          <Popover.Panel className="absolute right-0 z-10 mr-3 -mt-2.5 origin-top-right rounded-md bg-myDark1 p-4 shadow-2xl ring-1 ring-black  focus:outline-none">
+                            <form className="space-y-2">
                               {section.options.map((option, optionIdx) => (
                                 <div
                                   key={option.value}
@@ -195,11 +218,11 @@ export default function Filters() {
                                     type="checkbox"
                                     onChange={handlecategory}
                                     defaultChecked={option.checked}
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                    className="h-4 w-4 rounded border-myGrey text-indigo-600 focus:ring-indigo-500"
                                   />
                                   <label
                                     htmlFor={`filter-${section.id}-${optionIdx}`}
-                                    className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-gray-900"
+                                    className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-myGrey"
                                   >
                                     {option.label}
                                   </label>
@@ -216,7 +239,7 @@ export default function Filters() {
             </div>
           </div>
         </section>
-        <div>
+        {/* <div>
           <fieldset className="mt-4 ">
             <legend className="sr-only">Notification method</legend>
             <div className="space-x-20 flex justify-center">
@@ -248,6 +271,59 @@ export default function Filters() {
               ))}
             </div>
           </fieldset>
+        </div> */}
+
+        <div class=" mt-3">
+          <ul class="flex justify-between font-medium mt-0 mr-6 space-x-12 text-1xl">
+            <li>
+              <p
+                onClick={() => setSort("The Earliest")}
+                class="text-myGrey  hover:underline hover:text-myTeal1 "
+              >
+                The Earliest
+              </p>
+            </li>
+            <li>
+              <p
+                onClick={() => setSort("The Latest")}
+                class="text-myGrey dark:text-white hover:underline hover:text-myTeal1"
+              >
+                The Latest
+              </p>
+            </li>
+            <li>
+              <p
+                onClick={() => setSort("The Longest")}
+                class="text-myGrey  hover:underline hover:text-myTeal1"
+              >
+                The Longest
+              </p>
+            </li>
+            <li>
+              <p
+                onClick={() => setSort("The Shortest")}
+                class="text-myGrey   hover:underline hover:text-myTeal1"
+              >
+                The Shortest
+              </p>
+            </li>
+            <li>
+              <p
+                onClick={() => setSort("More Capacity")}
+                class="text-myGrey   hover:underline hover:text-myTeal1"
+              >
+                More Capacity
+              </p>
+            </li>
+            <li>
+              <p
+                onClick={() => setSort("Less Capacity")}
+                class="text-myGrey   hover:underline hover:text-myTeal1"
+              >
+                Less Capacity
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
