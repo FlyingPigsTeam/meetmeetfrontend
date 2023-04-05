32162b13286b7b91069b3321b4eb351dd2c31efd
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-04-05 13:23:09 +0430

    chore: make add room & info tab compoonents

---
+++ src/pages/Room/InfoTab/InfoTab.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+import SidebarNavigation from "../../../components/SidebarNavigation";
+import Header from "../../../components/Header";
+import InformationForm from "./InformationForm";
+import AddRoom from "../AddRoom";
+import Avatar200x200 from "../../../assets/images/200x200.png";
+import TestInf from "./TestInf";
+
+const InfoTab = () => {
+  return (
+    <>
+      <div id="root" class="min-h-100vh flex grow bg-slate-50 dark:bg-navy-900">
+        {/* <div class="flex items-center space-x-4 py-5 lg:py-6">
+          <h2 class="text-xl font-medium text-slate-800 dark:text-navy-50 lg:text-2xl">
+            Form Layout 5
+          </h2>
+          <div class="hidden h-full py-1 sm:flex">
+            <div class="h-full w-px bg-slate-300 dark:bg-navy-600"></div>
+          </div>
+          <ul class="hidden flex-wrap items-center space-x-2 sm:flex">
+            <li class="flex items-center space-x-2">
+              <a
+                class="text-primary transition-colors hover:text-primary-focus dark:text-accent-light dark:hover:text-accent"
+                href="#"
+              >
+                Forms
+              </a>
+              <svg
+                x-ignore
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </li>
+            <li>Form Layout 5</li>
+          </ul>
+        </div> */}
+        {/* <Header />
+      <div
+        style={{
+          paddingTop: "12vh",
+          display: "grid",
+          gridTemplateColumns: "1fr 4fr",
+        }}
+      >
+        <SidebarNavigation />
+        <div
+          className="bg-cyan-900 text-left"
+          style={{ width: "85vw", marginLeft: "13.9vw" }}
+        > */}
+        <main class="main-content w-full px-[var(--margin-x)] pb-8">
+          {/* <TestInf /> */}
+          <div class="grid grid-cols-12 gap-4 sm:gap-5 lg:gap-6">
+            <div class="col-span-12 lg:col-span-6">
+              {/* <VerticalNav />
+              <AddRoom /> */}
+                <TestInf />
+            </div>
+            <div class="col-span-12 lg:col-span-6">
+              {/* <div class="card"> */}
+                <InformationForm Title="Edit" />
+              {/* </div> */}
+            </div>
+          </div>
+        </main>
+        {/* </div>
+      </div> */}
+      </div>
+    </>
+  );
+};
+
+export default InfoTab;
+
+const VerticalNav = () => {
+  return (
+    <>
+      <div class="card p-4 sm:p-5">
+        <div class="flex items-center space-x-4">
+          <div class="avatar h-14 w-14">
+            <img class="rounded-full" src={Avatar200x200} alt="avatar" />
+          </div>
+          <div>
+            <h3 class="text-base font-medium text-slate-700 dark:text-navy-100">
+              Travis Fuller
+            </h3>
+            <p class="text-xs+ text-left">Admin</p>
+          </div>
+        </div>
+        <ul class="mt-6 space-y-1.5 font-inter font-medium">
+          <li>
+            <a
+              class="flex items-center space-x-2 rounded-lg bg-primary px-4 py-2.5 tracking-wide text-white outline-none transition-all dark:bg-accent"
+              href="#"
+            >
+              <i class="fa-solid fa-circle-info "></i>
+
+              <span>Information</span>
+            </a>
+          </li>
+          <li>
+            <a
+              class="group flex space-x-2 rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100"
+              href="#"
+            >
+              <i class="fa-solid fa-users"></i>
+
+              <span>Members</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/Room/InfoTab/TestInf.js
@@ -0,0 +1,249 @@
+import { PaperClipIcon } from "@heroicons/react/20/solid";
+import Avatar200x200 from "../../../assets/images/200x200.png";
+
+export default function TestInf() {
+  return (
+    <>
+      <div className="text-left p-6">
+        <div class="flex flex-col items-center space-y-4 border-b border-slate-200 p-4 dark:border-navy-500 sm:flex-row sm:justify-between sm:space-y-0 sm:px-5">
+          <h2 class="text-lg font-medium tracking-wide text-slate-700 dark:text-navy-100">
+            Room Information
+          </h2>
+          <div class="flex justify-center space-x-2">
+            <button class="btn min-w-[7rem] rounded-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+              Edit
+            </button>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="avatar mt-1.5 h-20 w-20">
+            <img class="mask is-squircle" src={Avatar200x200} alt="avatar" />
+          </div>
+        </div>
+        <div className="mt-5 border-t border-gray-200">
+          <dl className="divide-y divide-gray-200">
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Title</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">Gorzali</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Status</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow space-x-2 space-y-2">
+                  {" "}
+                  <div class="badge space-x-2 bg-slate-150 text-slate-800 dark:bg-navy-500 dark:text-navy-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Default</span>
+                  </div>
+                  <div class="badge space-x-2 bg-primary text-white dark:bg-accent">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <span>Primary</span>
+                  </div>
+                  <div class="badge space-x-2 bg-secondary text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Secondary</span>
+                  </div>
+                  <div class="badge rounded-full bg-info text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M5.5 16a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 16h-8z"></path>
+                    </svg>
+                  </div>
+                  <div class="badge bg-success text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="badge space-x-2 bg-warning text-white shadow-soft shadow-warning/50">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <span>Warning</span>
+                  </div>
+                  <div class="badge space-x-2 bg-error text-white">
+                    <span>Remove</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
+                </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Password</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">Backend Developer</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Categories</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">margotfoster@example.com</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Max members</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">$120,000</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">
+                Start Date & End Date
+              </dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">Backend Developer</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Description</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">
+                  Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+                  incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+                  consequat sint. Sit id mollit nulla mollit nostrud in ea
+                  officia proident. Irure nostrud pariatur mollit ad adipisicing
+                  reprehenderit deserunt qui eu.
+                </span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+              <dt className="text-sm font-medium text-gray-500">Categories</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <span className="flex-grow">margotfoster@example.com</span>
+                <span className="ml-4 flex-shrink-0">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Update
+                  </button>
+                </span>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </>
+  );
+}
