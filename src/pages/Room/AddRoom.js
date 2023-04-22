9732bd24931522bc7dd950fd7cac0c74b04e7e81
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-04-22 15:58:37 +0430

    fix: update AddRoom.js

--- src/pages/Room/AddRoom.js
+++ src/pages/Room/AddRoom.js
@@ -56,7 +56,6 @@ export default function AddRoom() {
 
       <button
         onClick={() => setIsOpen(true)}
-        // @click="showModal = true"
         class="btn bg-slate-150 font-medium text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
       >
         Origin Top
