b2532f41ba4c608875e9124e7eb92459aea612f9
matin2001 <matin1380day@gmail.com>
2023-05-06 22:19:57 +0430

    Chore: chat ui added to the project

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -6,6 +6,7 @@ import Sidebar from "../../components/Sidebar";
 import DarkModeToggle from "../../components/DarkModeToggle";
 import MainSection from "../../components/MainSection";
 import { useParams } from "react-router-dom";
+import Avatar200x200 from "../../assets/images/200x200.png";
 
 const Chat = () => {
   const params = useParams();
@@ -13,18 +14,21 @@ const Chat = () => {
   const client = new w3cwebsocket(`ws://localhost:8080/${roomId}`);
 
   const [message, setmessage] = useState([]);
-
+  const [payam, setpayam] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log(event.target.msg.value);
     // console.log(event.target.id.value);
-    client.send(
-      JSON.stringify({
-        username: "sobhankazemi",
-        user_id: parseInt(event.target.id.value),
-        message: event.target.msg.value,
-      })
-    );
+    if (payam != "") {
+      client.send(
+        JSON.stringify({
+          username: "sobhankazemi",
+          user_id: 1,
+          message: payam,
+        })
+      );
+    }
+    setpayam("");
   };
 
   useEffect(() => {
@@ -41,6 +45,28 @@ const Chat = () => {
     };
   }, [client.onmessage, client.onopen, client.onclose]);
 
+  const fake_data = [
+    {
+      username: "sobhankazemi",
+      user_id: 1,
+      message: "Hello to every one in this chat",
+    },
+    {
+      username: "matin2001",
+      user_id: 2,
+      message: "Hello to every one in this chat\nthis is the second message",
+    },
+    {
+      username: "sobhankazemi",
+      user_id: 1,
+      message: "How are you?",
+    },
+    {
+      username: "mohammadHossein",
+      user_id: 3,
+      message: "Hi, Guys",
+    },
+  ];
   console.log(message);
   return (
     <>
@@ -110,84 +136,97 @@ const Chat = () => {
             </Sidebar.Secondary.Minimized>
           </Sidebar.Secondary>
         </Sidebar>
-        <MainSection>
+        <MainSection className="w-full">
           <main
-            className={
-              "main-content h-100vh chat-app mt-0 flex w-full flex-col"
-            }
+            className={"mt-0 flex flex-col"}
+            style={{ height: "80vh", width: "107%" }}
           >
-            <Header>
-              <Header.Items>
-                <Header.SidebarToggle />
-                <Header.Right>
-                  <DarkModeToggle />
-                </Header.Right>
-              </Header.Items>
-            </Header>
             <div class="grow overflow-y-auto px-[calc(var(--margin-x)-.5rem)] py-5 transition-all duration-[.25s]">
-              <div
-                // x-transition:enter="transition-all duration-500 easy-in-out"
-                // x-transition:enter-start="opacity-0 [transform:translate3d(0,1rem,0)]"
-                // x-transition:enter-end="opacity-100 [transform:translate3d(0,0,0)]"
-                className="space-y-5"
-              >
+              <div className="space-y-2">
                 <div className="mx-4 flex items-center space-x-3">
-                  <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
+                  {/* <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
                   <p>Yesterday</p>
-                  <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
+                  <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div> */}
                 </div>
-
-                <div className="flex items-start space-x-2.5 sm:space-x-5">
-                  <div className="avatar">
-                    <img
-                      className="rounded-full"
-                      src={Avatar200x200}
-                      alt="avatar"
-                    />
-                  </div>
-
-                  <div className="flex flex-col items-start space-y-3.5">
-                    <div className="mr-4 max-w-lg sm:mr-10">
-                      <div className="rounded-2xl rounded-tl-none bg-white p-3 text-slate-700 shadow-sm dark:bg-navy-700 dark:text-navy-100">
-                        Hello My Dear. Lorem ipsum dolor sit amet, consectetur.
+                {message.map((item) => (
+                  <div>
+                    {item.username != "matin2001" ? (
+                      <div className="flex items-start space-x-2.5 sm:space-x-5">
+                        <div className="avatar">
+                          <img
+                            className="rounded-full"
+                            src={Avatar200x200}
+                            alt="avatar"
+                          />
+                        </div>
+                        <div className="flex flex-col items-start space-y-3.5">
+                          <div className="mr-4 max-w-lg sm:mr-10">
+                            <div className=" text-left text-md">
+                              {item.username}:
+                            </div>
+                            <div className="rounded-2xl text-left rounded-tl-none bg-white p-2 text-slate-700 shadow-sm dark:bg-navy-700 dark:text-navy-100">
+                              {item.message}
+                            </div>
+                            <p className="mt-1 ml-auto text-right text-xs text-slate-400 dark:text-navy-300">
+                              {item.time}
+                            </p>
+                          </div>
+                        </div>
                       </div>
-                      <p className="mt-1 ml-auto text-right text-xs text-slate-400 dark:text-navy-300">
-                        08:16
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex items-start justify-end space-x-2.5 sm:space-x-5">
-                  <div className="flex flex-col items-end space-y-3.5">
-                    <div className="ml-4 max-w-lg sm:ml-10">
-                      <div className="rounded-2xl rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Assumenda necessitatibus, ratione. Voluptatum.
-                      </div>
-                    </div>
-                    <div className="ml-4 max-w-lg sm:ml-10">
-                      <div className="rounded-2xl rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white">
+                    ) : (
+                      <div className="flex items-start justify-end space-x-2.5 sm:space-x-5">
+                        <div className="flex flex-col items-end space-y-3.5">
+                          <div className="ml-4 max-w-lg sm:ml-10">
+                            <div className=" text-left text-md">
+                              {item.username}:
+                            </div>
+                            <div className="rounded-2xl text-left rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white">
+                              {item.message}
+                            </div>
+                          </div>
+                          <div className="ml-4 max-w-lg sm:ml-10">
+                            {/* <div className="rounded-2xl rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white">
                         And that’s why a 15th century
+                      </div> */}
+                            <p className="mt-1 ml-auto text-left text-xs text-slate-400 dark:text-navy-300">
+                              {item.time}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="avatar">
+                          <img
+                            className="rounded-full"
+                            src={Avatar200x200}
+                            alt="avatar"
+                          />
+                        </div>
                       </div>
-                      <p className="mt-1 ml-auto text-left text-xs text-slate-400 dark:text-navy-300">
-                        08:16
-                      </p>
-                    </div>
+                    )}
                   </div>
-                  <div className="avatar">
-                    <img
-                      className="rounded-full"
-                      src={Avatar200x200}
-                      alt="avatar"
-                    />
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
-            <div className="chat-footer relative flex h-12 w-full shrink-0 items-center justify-between border-t border-slate-150 bg-white px-[calc(var(--margin-x)-.25rem)] transition-[padding,width] duration-[.25s] dark:border-navy-600 dark:bg-navy-800">
-              <div className="-ml-1.5 flex flex-1 space-x-2">
-                <button className="btn h-9 w-9 shrink-0 rounded-full p-0 text-slate-500 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:text-navy-200 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
+            <div
+              style={{ width: "89%" }}
+              className="chat-footer fixed bottom-0  h-12 border-slate-150 bg-white transition-[padding,width] duration-[.25s] dark:border-navy-600 dark:bg-navy-800"
+            >
+              <form
+                onSubmit={handleSubmit}
+                className="flex items-center justify-between"
+              >
+                <input
+                  value={payam}
+                  className="form-input h-12 border-none bg-transparent placeholder:text-slate-400/70 focus:border-navy-50 outline-none "
+                  placeholder="Write the message"
+                  style={{ width: "95%" }}
+                  onChange={(e) => setpayam(e.target.value)}
+                />
+                <button
+                  type="submit"
+                  value="submit"
+                  style={{ marginRight: "2vw" }}
+                  className="btn h-9 w-9 shrink-0 rounded-full p-0 text-primary hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:text-accent-light dark:hover:bg-accent-light/20 dark:focus:bg-accent-light/20 dark:active:bg-accent-light/25"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5.5 w-5.5"
@@ -199,35 +238,23 @@ const Chat = () => {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                      d="m9.813 5.146 9.027 3.99c4.05 1.79 4.05 4.718 0 6.508l-9.027 3.99c-6.074 2.686-8.553.485-5.515-4.876l.917-1.613c.232-.41.232-1.09 0-1.5l-.917-1.623C1.26 4.66 3.749 2.46 9.813 5.146ZM6.094 12.389h7.341"
                     />
                   </svg>
                 </button>
+              </form>
+            </div>
 
+            {/* <div className="chat-footer fixed bottom-0 flex h-12  items-center justify-between border-slate-150 bg-white transition-[padding,width] duration-[.25s] dark:border-navy-600 dark:bg-navy-800">
+              <div style={{ width: "82vw" }}>
                 <input
                   type="text"
-                  className="form-input h-9 border-none w-full bg-transparent placeholder:text-slate-400/70"
+                  className="form-input h-12 border-none w-full bg-transparent placeholder:text-slate-400/70"
                   placeholder="Write the message"
                 />
               </div>
 
-              <div className="-mr-1.5 flex">
-                <button className="btn h-9 w-9 shrink-0 rounded-full p-0 text-slate-500 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:text-navy-200 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5.5 w-5.5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </button>
+              <div>
                 <button className="btn h-9 w-9 shrink-0 rounded-full p-0 text-primary hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:text-accent-light dark:hover:bg-accent-light/20 dark:focus:bg-accent-light/20 dark:active:bg-accent-light/25">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -245,8 +272,8 @@ const Chat = () => {
                   </svg>
                 </button>
               </div>
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
               <form onSubmit={handleSubmit}>
                 <input
                   type="text"
@@ -259,7 +286,7 @@ const Chat = () => {
                   Submit
                 </button>
               </form>
-            </div>
+            </div> */}
           </main>
         </MainSection>
       </PageWrapper>
