32d227ce1c46ba23db13573d4a9a442aa9e579d2
matin2001 <matin1380day@gmail.com>
2023-05-23 15:11:31 +0430

    Fix: Multiple Lines error and other errors in chat fixed

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -8,6 +8,8 @@ import MainSection from "../../components/MainSection";
 import { useParams } from "react-router-dom";
 import Avatar200x200 from "../../assets/images/200x200.png";
 import AuthContext from "../../context/AuthContext";
+import { data } from "browserslist";
+import { async } from "q";
 
 const Chat = () => {
   const params = useParams();
@@ -19,28 +21,44 @@ const Chat = () => {
   const messageEndRef = useRef(null);
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(event.target[0].value);
     // console.log(event.target.msg.value);
     // console.log(event.target.id.value);
-    if (payam != "") {
+    if (event.target[0].value != "") {
       client.send(
         JSON.stringify({
           username: user.username,
           user_id: user.user_id,
-          message: payam,
+          message: event.target[0].value,
         })
       );
     }
-    setpayam("");
+    //setpayam("");
+    event.target[0].value = "";
   };
+  function insertSpaceEvery70Words(words) {
+    const wordsWithSpace = [];
+    for (let i = 0; i < words.length; i++) {
+      wordsWithSpace.push(words[i]);
+      if ((i + 1) % 20 === 0) {
+        wordsWithSpace.push(" ");
+      }
+    }
+    return wordsWithSpace.join("");
+  }
 
   useEffect(() => {
     client.onopen = () => {
       console.log("WebSocket Client Connected");
     };
     client.onmessage = (message) => {
-      const dataFromServer = JSON.parse(message.data);
+      let dataFromServer = JSON.parse(message.data);
+      // if (!dataFromServer.message.includes(" ")) {
+      //   const outputString = insertSpaceEvery70Words(dataFromServer.message);
+      //   dataFromServer.message = outputString;
+      // }
       setmessage((e) => [...e, dataFromServer]);
-      console.log("got reply! ", dataFromServer);
+      //console.log("got reply! ", dataFromServer);
     };
     client.onclose = () => {
       console.log("WebSocket Client disConnected");
@@ -50,8 +68,15 @@ const Chat = () => {
     messageEndRef.current?.scrollIntoView({ block: "end" });
   }, [message]);
 
-  console.log(message);
-  console.log(user);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = async (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  };
+
+  // console.log(message);
+  // console.log(user);
   return (
     <>
       <PageWrapper>
@@ -132,56 +157,70 @@ const Chat = () => {
                   <p>Yesterday</p>
                   <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div> */}
                 </div>
-                {message.length != 0
-                  ? message.map((item, index) => (
-                      <div key={index}>
-                        {item.username != user.username ? (
-                          <div className="flex items-start space-x-2.5 sm:space-x-5">
-                            <div className="avatar">
-                              <img
-                                className="rounded-full"
-                                src={Avatar200x200}
-                                alt="avatar"
-                              />
-                            </div>
-                            <div className="flex flex-col items-start space-y-3.5">
-                              <div className="mr-4 max-w-lg sm:mr-10">
-                                <div className=" text-left text-md  text-slate-600 dark:text-navy-200">
-                                  {item.username}:
-                                </div>
-                                <div className="rounded-2xl text-left rounded-tl-none bg-slate-200 p-2 text-slate-900 shadow-sm dark:bg-navy-700 dark:text-navy-50">
-                                  {item.message}
-                                </div>
-                                <p className="mt-1 ml-auto text-right text-xs text-slate-400 dark:text-navy-300">
-                                  {item.time.split(" ")[1]}
-                                </p>
+                {message.length != 0 ? (
+                  message.map((item, index) => (
+                    <div key={index}>
+                      {item.username != user.username ? (
+                        <div className="flex items-start space-x-2.5 sm:space-x-5">
+                          <div className="avatar">
+                            <img
+                              className="rounded-full"
+                              src={Avatar200x200}
+                              alt="avatar"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start space-y-3.5">
+                            <div className="mr-4 max-w-lg sm:mr-10">
+                              <div className=" text-left text-md  text-slate-600 dark:text-navy-200">
+                                {item.username}:
                               </div>
+                              <div
+                                style={{
+                                  whiteSpace: "initial",
+                                  wordWrap: "break-word",
+                                }}
+                                className="rounded-2xl text-left rounded-tl-none bg-slate-200 p-2 text-slate-900 shadow-sm dark:bg-navy-700 dark:text-navy-50"
+                              >
+                                <p> {item.message}</p>
+                              </div>
+                              <p className="mt-1 ml-auto text-right text-xs text-slate-400 dark:text-navy-300">
+                                {item.time.split(" ")[1]}
+                              </p>
                             </div>
                           </div>
-                        ) : (
-                          <div className="flex items-start justify-end space-x-2.5 sm:space-x-5">
-                            <div className="flex flex-col items-end space-y-3.5">
-                              <div className="ml-4 max-w-lg sm:ml-10">
-                                <div className="rounded-2xl text-left rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white">
-                                  {item.message}
-                                </div>
-                                <p className="mt-1 ml-4 max-w-lg sm:ml-10 text-left text-xs text-slate-400 dark:text-navy-300">
-                                  {item.time.split(" ")[1]}
-                                </p>
+                        </div>
+                      ) : (
+                        <div className="flex items-start justify-end space-x-2.5 sm:space-x-5">
+                          <div className="flex flex-col items-end space-y-3.5">
+                            <div className="ml-4 max-w-lg sm:ml-10">
+                              <div
+                                style={{
+                                  whiteSpace: "initial",
+                                  wordWrap: "break-word",
+                                }}
+                                className=" text-ellipsis rounded-2xl text-left rounded-tr-none bg-info/10 p-3 text-slate-700 shadow-sm dark:bg-accent dark:text-white"
+                              >
+                                <p>{item.message}</p>
                               </div>
-                            </div>
-                            <div className="avatar">
-                              <img
-                                className="rounded-full"
-                                src={Avatar200x200}
-                                alt="avatar"
-                              />
+                              <p className="mt-1 ml-4 max-w-lg sm:ml-10 text-left text-xs text-slate-400 dark:text-navy-300">
+                                {item.time.split(" ")[1]}
+                              </p>
                             </div>
                           </div>
-                        )}
-                      </div>
-                    ))
-                  : <div>Start Chatting Here</div>}
+                          <div className="avatar">
+                            <img
+                              className="rounded-full"
+                              src={Avatar200x200}
+                              alt="avatar"
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ))
+                ) : (
+                  <div>Start Chatting Here</div>
+                )}
               </div>
               <div ref={messageEndRef} />
             </div>
@@ -190,6 +229,41 @@ const Chat = () => {
               className="chat-footer fixed bottom-0 bg-slate-200  h-12 border-slate-150 transition-[padding,width] duration-[.25s] dark:border-navy-600 dark:bg-navy-800"
             >
               <form
+                onSubmit={handleSubmit}
+                id="form"
+                className="flex items-center justify-between "
+              >
+                <input
+                  type="text"
+                  id="msg"
+                  size="64"
+                  className="form-input h-12 border-none bg-slate-200 dark:bg-navy-800 placeholder:text-slate-400/70 focus:border-navy-50 outline-none "
+                  placeholder="Write the message"
+                  style={{ width: "95%" }}
+                />
+                <button
+                  type="submit"
+                  value="send"
+                  style={{ marginRight: "2vw" }}
+                  className="btn h-9 w-9 shrink-0 rounded-full p-0 text-primary hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:text-accent-light dark:hover:bg-accent-light/20 dark:focus:bg-accent-light/20 dark:active:bg-accent-light/25"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5.5 w-5.5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m9.813 5.146 9.027 3.99c4.05 1.79 4.05 4.718 0 6.508l-9.027 3.99c-6.074 2.686-8.553.485-5.515-4.876l.917-1.613c.232-.41.232-1.09 0-1.5l-.917-1.623C1.26 4.66 3.749 2.46 9.813 5.146ZM6.094 12.389h7.341"
+                    />
+                  </svg>
+                </button>
+              </form>
+              {/* <form
                 onSubmit={handleSubmit}
                 className="flex items-center justify-between "
               >
@@ -221,7 +295,7 @@ const Chat = () => {
                     />
                   </svg>
                 </button>
-              </form>
+              </form> */}
             </div>
 
             {/* <div className="chat-footer fixed bottom-0 flex h-12  items-center justify-between border-slate-150 bg-white transition-[padding,width] duration-[.25s] dark:border-navy-600 dark:bg-navy-800">
