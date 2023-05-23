b634967db0b54d759a2bd5c30453361cc52b02af
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-05-23 19:38:55 +0330

    feature: create subscribtion page

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -25,7 +25,7 @@ import Task from "../pages/Room/Task/Task";
 import Chat from "../pages/Chat/Chat";
 import Chat_test from "../pages/Chat/Chat_test";
 import JoinRoom from "../pages/Room/InfoTab/joinRoom";
-
+import Subscribtion from "../pages/Subscribtion/Subscribtion";
 const Router = () => {
   return (
     <div>
@@ -47,7 +47,7 @@ const Router = () => {
           <Route path="/EditProfile" element={<EditProfile />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/profileEdit" element={<Setting />} />
-
+          <Route path="/subscribtion" element={<Subscribtion />} />
           {/* <Route path="/room/info" element={<InfoTab />} /> */}
           <Route path="/room/:idroom/info" element={<InfoTab />} />
           <Route
