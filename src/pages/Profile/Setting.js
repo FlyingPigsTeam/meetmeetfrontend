2fed0dd1c54eebe43588e442d522cff7b2cc1cb8
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-05-25 12:17:05 +0330

    fix: add File TypeChecking on upload

--- src/pages/Profile/Setting.js
+++ src/pages/Profile/Setting.js
@@ -202,8 +202,13 @@ export default function Setting({user, onCancel}) {
 
                                                             <label htmlFor={"edit-avatar-btn"}
                                                                    className="btn h-6 w-6 rounded-full border border-slate-200 p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:border-navy-500 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-                                                                <input type="file" id="edit-avatar-btn"
-                                                                       onChange={onSelectImage} hidden/>
+                                                                <input 
+                                                                    id="edit-avatar-btn"
+                                                                    type="file" 
+                                                                    accept=".jpg, .jpeg, .png"
+                                                                    onChange={onSelectImage} 
+                                                                    hidden
+                                                                />
                                                                 <svg
                                                                     xmlns="http://www.w3.org/2000/svg"
                                                                     className="h-3.5 w-3.5"
