d233edc6bb7ccdb64ea12e704b5197120297c340
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-05-30 11:41:18 +0330

    feat : fix animation

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,10 +1,10 @@
 const Skeleton = ({ members }) => {
   return (
     <div>
-      <div className="flex animate-pulse flex-wrap -space-x-2">
+      <div className="flex flex-wrap -space-x-2">
         {members.map((item, index) => (
-          <div className="avatar h-8 w-8" key={index}>
-            {item.picture_path ? (
+          <div className="avatar h-8 w-8 hover:z-10" key={index}>
+            {item.picture_path  && item.picture_path!="" && item.picture_path!="__"? (
               <img
                 className="rounded-full ring ring-white dark:ring-navy-700"
                 src={item.picture_path}
@@ -23,3 +23,22 @@ const Skeleton = ({ members }) => {
 };
 
 export default Skeleton;
+
+
+
+{/* <div class="flex flex-wrap -space-x-2">
+    <div class="avatar h-8 w-8 hover:z-10">
+      <img
+        class="rounded-full ring ring-white dark:ring-navy-700"
+        src="images/200x200.png"
+        alt="avatar"
+      />
+    </div>
+
+    <div class="avatar h-8 w-8 hover:z-10">
+      <div
+        class="is-initial rounded-full bg-info text-xs+ uppercase text-white ring ring-white dark:ring-navy-700"
+      >
+        jd
+      </div> */}
+    // </div>
