5db0b5ca6f0754cd2c7cdf6c7d8551ee414d3c14
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-07-07 19:22:49 +0330

    feat: url in subscribtion

--- src/pages/Subscribtion/Subscribtion.js
+++ src/pages/Subscribtion/Subscribtion.js
@@ -39,7 +39,7 @@ const Subscribtion = () => {
     const handleGetAccess = async () => {
         try {
             await axios
-                .put("http://127.0.0.1:8000/api/premium");
+                .put("api/premium");
                 
 
         } catch (error) {
