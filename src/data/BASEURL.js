00063e57693a5febd7b3ef395e8d3849b4c583c3
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-07-09 00:37:49 +0330

    fix: RQ debugged removed

--- src/data/BASEURL.js
+++ src/data/BASEURL.js
@@ -1,3 +1,6 @@
 // export const BASEURL = "http://127.0.0.1:8000";
 export const BASEURL = "https://meet-meet.ir/backend";
 export const FrontURL = "http://localhost:3000";
+
+
+export const RQ_Debug = false;
